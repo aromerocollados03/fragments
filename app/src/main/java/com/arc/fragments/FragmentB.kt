12b2e3eb2b9fedf3dc9f066dc17f2cf461d5b522
Alejandro Romero <alejandroromerocollados.alu@iespacomolla.es>
2023-11-13 12:45:01 +0100

    final commit

--- app/src/main/java/com/arc/fragments/FragmentB.kt
+++ app/src/main/java/com/arc/fragments/FragmentB.kt
@@ -1,5 +1,6 @@
 package com.arc.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 
 class FragmentB : Fragment() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
